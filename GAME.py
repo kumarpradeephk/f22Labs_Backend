303749d7885c5b72ab4cd8c9cbd79c3366305f24
Pradeep <harendra179@gmail.com>
2018-01-25 13:06:30 +0530

    F22

---
+++ GAME.py
@@ -0,0 +1,51 @@
+import random
+from random import randint
+import string
+#RANDOM STRING GENERATOR 
+def generate_random_str(randomNum):
+  randomString=''.join([random.choice(string.ascii_uppercase) for _ in range(randomNum)])
+  return randomString
+
+#PROCESS_BOTH_STRING
+def process_both_string(randomString,userStr,randomNum):
+  exact_matchNo=0
+  for i in range(randomNum):
+    if randomString[i]==userStr[i]:
+      exact_matchNo=exact_matchNo+1
+      
+  randomString_hash={}
+  userStr_hash={}
+  for i in range(randomNum):
+    counts=0
+    counts=randomString.count(randomString[i])
+    randomString_hash[randomString[i]]=counts
+    counts=0
+    counts=userStr.count(userStr[i])
+    userStr_hash[userStr[i]]=counts
+  
+  match_val=0
+  for k in userStr_hash.keys():
+    if k in randomString_hash.keys():
+      if randomString_hash[k]>=userStr_hash[k]:
+        match_val=match_val+userStr_hash[k]
+      else:
+        match_val=match_val+randomString_hash[k]
+  
+  char_match=match_val-exact_matchNo
+  print("Number of charcter matches with EXACT_POSITION:= " + str(exact_matchNo))
+  print("Number of charcter matches with WRONG_POSITION:= " + str(char_match))
+
+#DRIVER_PROGRAMME  
+rNum=randint(2,20);
+#USER GUESSING STATS
+randString=generate_random_str(rNum)
+usrStr=input("enter string of length "+ str(rNum)+ ":-")
+print(".......PLAYER GUESSING STATS........")
+process_both_string(randString,usrStr,rNum)
+
+print("---------------------------------------------------")
+#SYSTEM GUESSING STATS
+Sys_randString=generate_random_str(rNum)
+Sys_user_String=generate_random_str(rNum)
+print(".......SYSTEM GUESSING STATS........")
+process_both_string(Sys_randString,Sys_user_String,rNum)
---
+++ data_fetch_from_URL.py
@@ -0,0 +1,28 @@
+import requests
+r = requests.get("https://gist.githubusercontent.com/murtuzakz/4bd887712703ff14c9b0f7c18229b332/raw/d0dd1c59016e2488dcbe0c8e710a1c5df9c3672e/season7.json")
+rjson = r.json()
+
+
+investor_to_product = {}
+
+for key in rjson.keys():
+	for json in rjson[key]:
+		invest = json['investors']
+		product = json['product']
+		product = product.replace('\n','')
+		#process invest
+		invest_list = invest.replace(',',' and ').split(' and ')
+
+		for invest1 in invest_list:
+			invest1 = invest1.strip()
+			invest1 = invest1.replace('\n','').replace('O\u2019','').replace('  ',' ')
+			if invest1 not in investor_to_product.keys() and invest1 != '':
+				investor_to_product[invest1] = []
+			if invest1!='':
+				investor_to_product[invest1].append(product)
+
+dict_items = sorted(investor_to_product.items(),key=lambda x:len(x[1]))
+i=1
+for k,v in dict_items:
+    print(str(i)+". "+k+" : "+str(v)+"\n")
+    i=i+1
